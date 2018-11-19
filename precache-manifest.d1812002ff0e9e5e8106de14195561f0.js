9476478437044ff6d538d60d7e4cb0708998384e
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-11-19 13:39:16 -0500

    Updates

---
+++ precache-manifest.d1812002ff0e9e5e8106de14195561f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/My-Reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/My-Reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/My-Reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "6577964ed44e9d458ff4",
+    "url": "/My-Reads/static/js/runtime~main.6577964e.js"
+  },
+  {
+    "revision": "e9243b99d91ade279106",
+    "url": "/My-Reads/static/js/main.e9243b99.chunk.js"
+  },
+  {
+    "revision": "a6171ed51df0c3e8e9cd",
+    "url": "/My-Reads/static/js/1.a6171ed5.chunk.js"
+  },
+  {
+    "revision": "e9243b99d91ade279106",
+    "url": "/My-Reads/static/css/main.13883529.chunk.css"
+  },
+  {
+    "revision": "bb7456bc7eac544cfe57576eb83cdbeb",
+    "url": "/My-Reads/index.html"
+  }
+];
