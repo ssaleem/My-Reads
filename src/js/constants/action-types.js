2d3bff4b6bc830f547a0d978badf67e6c3f2af72
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2019-02-06 13:56:07 -0500

    Add action type constants

---
+++ src/js/constants/action-types.js
@@ -0,0 +1,5 @@
+export const Add_BOOK = 'ADD_BOOK';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
+export const GET_ALL_BOOKS = 'GET_ALL_BOOKS';
+export const BOOKS_LOADED = 'BOOKS_LOADED';
+export const SEARCH_BOOKS = 'SEARCH_BOOKS';
