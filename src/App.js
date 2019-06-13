caf29b69754dd4db1517bca8efaad98474b49da1
ssaleem <sara@sarasaleem.com>
2019-06-13 13:36:34 -0400

    Transform updateBook method to use arrow function and consequently binding 'this' t surrounding scope

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import * as BooksAPI from './utils/BooksAPI'
-import SearchBooks from './components/SearchBooks'
-import ListBooks from './components/ListBooks'
-import './App.css'
+import React from 'react';
+import {Route} from 'react-router-dom';
+import * as BooksAPI from './utils/BooksAPI';
+import SearchBooks from './components/SearchBooks';
+import ListBooks from './components/ListBooks';
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+  };
+
+  componentDidMount () {
+    BooksAPI.getAll ().then (books => this.setState ({books}));
   }
 
-  componentDidMount() {
-    BooksAPI.getAll().then(books => this.setState({ books: books }))
-  }
-
-  updateBook(book, newShelf) {
-    BooksAPI.update(book, newShelf)
-    .then( () => {
-      this.setState(prevState => {
-        let index = prevState.books.findIndex(bookEntry => bookEntry.id === book.id)
-        if(index !== -1) {
-          prevState.books[index].shelf = newShelf
-        }
-        else {
-          book.shelf = newShelf
-          prevState.books = prevState.books.concat(book)
-        }
-        return { state: prevState}
+  updateBook = (book, newShelf) => {
+    BooksAPI.update (book, newShelf)
+      .then(() => {
+        this.setState (prevState => {
+          let index = prevState.books.findIndex (
+            bookEntry => bookEntry.id === book.id
+          );
+          if (index !== -1) {
+            prevState.books[index].shelf = newShelf;
+          }
+          else {
+            book.shelf = newShelf;
+            prevState.books = prevState.books.concat (book);
+          }
+          return {state: prevState};
+        });
       })
-    })
-    .catch(() => console.log('book shelf update unsuccessful'));
-
+      .catch (e => console.log (e));
   }
 
-
-  render() {
+  render () {
     return (
-      <div className="app">
-          <Route exact path='/' render={() => (
-          <ListBooks
-            books={this.state.books}
-            onUpdateBook={(book, newShelf) => this.updateBook(book, newShelf)}
-          />
-        )}/>
-          <Route path="/search" render={() => (
+      <>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListBooks
+              books={this.state.books}
+              onUpdateBook={this.updateBook}
+            />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
             <SearchBooks
-            bookList={this.state.books}
-            onUpdateBook={(book, newShelf) => this.updateBook(book, newShelf)}/>
-          )}/>
-      </div>
-    )
+              bookList={this.state.books}
+              onUpdateBook={this.updateBook}
+            />
+          )}
+        />
+      </>
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
