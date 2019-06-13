73164bf7d3e7f64a187c755a225da91c1ea30f9e
ssaleem <sara@sarasaleem.com>
2019-06-13 14:50:55 -0400

    Minor code refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById ('root')
+);
